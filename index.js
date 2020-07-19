4e380782ada3963775ac74f95782352684bd0340
Ashtonn77 <ashtonrbay@gmail.com>
2020-07-19 21:48:19 +0200

    added fetch functionality and updated js

--- index.js
+++ index.js
@@ -48,16 +48,68 @@ function searchMeal(e) {
   }
 }
 
+//add meal to dom
+function addMealToDom(meal) {
+  const ingredients = [];
+
+  for (let i = 1; i < 20; i++) {
+    if (meal[`strIngredient${i}`]) {
+      ingredients.push(
+        `${meal[`strIngredient${i}`]} - ${meal[`strMeasure${i}`]}`
+      );
+    } else {
+      break;
+    }
+  }
+
+  singleMeal.innerHTML = `
+    <div class="single-meal">
+      <h1>${meal.strMeal}</h1>
+      <img src="${meal.strMealThumb}" alt="${meal.strMeal}"/>
+      <div class="single-meal-info">
+        ${meal.strCategory ? `<p>${meal.strCategory}</p>` : ""}
+        ${meal.strArea ? `<p>${meal.strArea}</p>` : ""}
+      </div>
+
+      <div class="main">
+      <p>${meal.strInstructions}</p>
+      <h2>Ingredients</h2>
+      <ul>
+      
+      ${ingredients.map((ing) => `<li>${ing}</li>`).join("")}
+      
+      </ul>
+      </div>
+
+    </div>
+  `;
+}
+
+//fecth meal by id
+function getMealById(mealId) {
+  fetch(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealId}`)
+    .then((res) => res.json())
+    .then((data) => {
+      const meal = data.meals[0];
+
+      addMealToDom(meal);
+    });
+}
+
 //event listeners
 submit.addEventListener("submit", searchMeal);
 
 mealsEl.addEventListener("click", (e) => {
   const mealInfo = e.path.find((item) => {
     if (item.classList) {
-      return (item.classList = "meal-info");
+      return item.classList.contains("meal-info");
     } else {
       return false;
     }
   });
-  console.log(mealInfo);
+
+  if (mealInfo) {
+    const mealId = mealInfo.getAttribute("data-mealId");
+    getMealById(mealId);
+  }
 });
